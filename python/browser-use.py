f06a9efabffea3cdb13b50f6016dc1e535364c1a
taijusanagi <taijusanagi@gmail.com>
2025-03-21 09:42:24 +1300

    extract playwright browser from browser-use

--- python/browser-use.py
+++ python/browser-use.py
@@ -1,16 +1,21 @@
 from langchain_openai import ChatOpenAI
-from browser_use import Agent
+from browser_use import Agent, Browser
 from dotenv import load_dotenv
-load_dotenv()
 
 import asyncio
+
+load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o")
 
 async def main():
+    browser = Browser()
+    playwright_browser = await browser.get_playwright_browser()
+    context = await playwright_browser.new_context()
     agent = Agent(
-        task="Compare the price of gpt-4o and DeepSeek-V3",
+        task="Go to https://metamask.github.io/test-dapp/ and check the wallet connection",
         llm=llm,
+        browser=browser
     )
     result = await agent.run()
     print(result)
