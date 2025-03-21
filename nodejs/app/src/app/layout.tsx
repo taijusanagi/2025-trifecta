6f7ba3d36ddccc0016805eacdef33684d2304ff2
taijusanagi <taijusanagi@gmail.com>
2025-03-21 19:12:00 +1300

    suppress error

--- nodejs/app/src/app/layout.tsx
+++ nodejs/app/src/app/layout.tsx
@@ -21,8 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+        suppressHydrationWarning
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
